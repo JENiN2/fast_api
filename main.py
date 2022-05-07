8a94f54575a1b7540d29827c411ef1d9e6a56256
JENiN2 <hlfan2@yandex.ru>
2022-05-07 17:31:55 +0700

    initial

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
 @app.get('/')
-def index():
-    return {'Data': {'Name': 'Yolo'}}
+def start():
+    return {'data': 'Hello!'}
 
 
-@app.get('/about')
-def about():
-    return {'Data': {'About Page'}}
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    # fetch comments of blog with id = id
+    return {'data': {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(fblog: Blog):
+    return {'data': f'blog is created width title as {fblog.title}'}
