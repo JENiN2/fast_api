070580cc4934190ff00e272bdfc14c92404da68b
JENiN2 <hlfan2@yandex.ru>
2022-06-27 00:10:50 +0700

    added creation time for blogs

--- blog_manager.py
+++ blog_manager.py
@@ -1,4 +1,5 @@
 from typing import Optional, List
+from datetime import datetime, timezone
 
 import asyncpg
 from pydantic import BaseModel
@@ -8,7 +9,8 @@ class Blog(BaseModel):
     id: Optional[int] = None
     title: str
     body: str
-    published: bool
+    created: str
+    published: bool    
                 
 
 class BlogManager:
@@ -17,24 +19,26 @@ class BlogManager:
 
     async def add_blog(self, blog: Blog):
         async with self.storage.acquire() as conn:
+            dt = datetime.now(timezone.utc)
+            print(dt)
             await conn.execute('''
-               INSERT INTO blogs (title, body, published) VALUES ($1, $2, $3)
-            ''', blog.title, blog.body, blog.published)                      
+               INSERT INTO blogs2 (title, body, created, published) VALUES ($1, $2, $3, $4)
+            ''', blog.title, blog.body, dt, blog.published)         
                 
     async def get_blog_by_id(self, blog_id: int) -> Optional[Blog]:
         async with self.storage.acquire() as conn:
             return await conn.fetch('''
-               SELECT * FROM blogs WHERE ID=($1)
+               SELECT * FROM blogs2 WHERE ID=($1)
             ''', blog_id)        
     
     async def remove_blog_by_id(self, blog_id: int):
         async with self.storage.acquire() as conn:
             await conn.execute('''
-               DELETE FROM blogs WHERE ID=($1)
+               DELETE FROM blogs2 WHERE ID=($1)
             ''', blog_id)    
 
     async def get_blogs(self) -> List[Blog]:
         async with self.storage.acquire() as conn:
             return await conn.fetch('''
-               SELECT * FROM blogs
+               SELECT * FROM blogs2
             ''')
